041ba4311003386d2ff226faaf9301d8f1c624ca
InvestUz <invest.abdusattorov@gmail.com>
2024-10-21 19:04:42 +0500

    working with fixing logos

---
+++ resources/views/inc/__partners_logos.blade.php
@@ -0,0 +1,85 @@
+<div class="home-banners d-none d-md-block">
+    <div class="home-banners_row _5-el">
+        <a class="home-banner" href="/fintech/market_place/" title="Проект «Маркетплейс»">
+            <div class="home-banner_image">
+                <img src="https://www.cbr.ru/StaticHtml/File/81950/mp.svg"
+                    alt="Проект «Маркетплейс»" />
+            </div>
+            <div class="home-banner_text">Маркетплейс</div>
+        </a><a class="home-banner" href="/PSystem/sfp/" title="Система быстрых платежей">
+            <div class="home-banner_image">
+                <img src="https://www.cbr.ru/StaticHtml/File/81950/sbp.svg"
+                    alt="Система быстрых платежей" />
+            </div>
+            <div class="home-banner_text">
+                Система быстрых платежей
+            </div>
+        </a><a class="home-banner" href="/dkp/mp/" title="Мониторинг предприятий">
+            <div class="home-banner_image">
+                <img src="https://www.cbr.ru/StaticHtml/File/81950/monitor.svg"
+                    alt="Мониторинг предприятий" />
+            </div>
+            <div class="home-banner_text">Мониторинг предприятий</div>
+        </a><a class="home-banner" href="/projects_xbrl/" title="Открытый стандарт отчетности">
+            <div class="home-banner_image">
+                <img src="https://www.cbr.ru/StaticHtml/File/81950/XBRL.svg"
+                    alt="Открытый стандарт отчетности" />
+            </div>
+            <div class="home-banner_text">
+                Открытый стандарт отчетности
+            </div>
+        </a>
+    </div>
+</div>
+
+<div class="home-banners-wrap d-block d-md-none">
+    <div class="swiper-container swiper-container-initialized swiper-container-horizontal swiper-container-android"
+        data-home-banners-gall="">
+        <div class="swiper-wrapper" style="transition-duration: 0ms">
+            <div class="swiper-slide">
+                <a class="home-banner" href="/fintech/market_place/" title="Маркетплейс">
+                    <div class="home-banner_image">
+                        <img src="https://www.cbr.ru/StaticHtml/File/81950/mp.svg"
+                            alt="Маркетплейс" />
+                    </div>
+                    <div class="home-banner_text">Маркетплейс</div>
+                </a>
+            </div>
+            <div class="swiper-slide">
+                <a class="home-banner" href="/PSystem/sfp/" title="Система быстрых платежей">
+                    <div class="home-banner_image">
+                        <img src="https://www.cbr.ru/StaticHtml/File/81950/sbp.svg"
+                            alt="Система быстрых платежей" />
+                    </div>
+                    <div class="home-banner_text">
+                        Система быстрых платежей
+                    </div>
+                </a>
+            </div>
+
+            <div class="swiper-slide">
+                <a class="home-banner" href="/dkp/mp/" title="Мониторинг предприятий">
+                    <div class="home-banner_image">
+                        <img src="https://www.cbr.ru/StaticHtml/File/81950/monitor.svg"
+                            alt="Открытый стандарт отчетности" />
+                    </div>
+                    <div class="home-banner_text">
+                        Мониторинг предприятий
+                    </div>
+                </a>
+            </div>
+            <div class="swiper-slide">
+                <a class="home-banner" href="/projects_xbrl/" title="Открытый стандарт отчетности">
+                    <div class="home-banner_image">
+                        <img src="https://www.cbr.ru/StaticHtml/File/81950/XBRL.svg"
+                            alt="Открытый стандарт отчетности" />
+                    </div>
+                    <div class="home-banner_text">
+                        Открытый стандарт отчетности
+                    </div>
+                </a>
+            </div>
+        </div>
+        <span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span>
+    </div>
+</div>
