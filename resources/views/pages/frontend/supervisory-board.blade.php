bd326a2e89586fd6ec2423b37ce530bfb3026e69
InvestUz <invest.abdusattorov@gmail.com>
2024-12-09 16:56:44 +0500

    upgrading blades

--- resources/views/pages/frontend/supervisory-board.blade.php
+++ resources/views/pages/frontend/supervisory-board.blade.php
@@ -7,7 +7,7 @@
                     <div class="breadcrumbs">
                         <div class="breadcrumbs_item"><a class="breadcrumbs_home" href="/"></a></div>
                         <div class="breadcrumbs_item">
-                            <a href="/about_br/">О Нас</a>
+                            <a href="{{ route('frontend.about') }}">О Нас</a>
                         </div>
                     </div>
 
@@ -18,35 +18,41 @@
                         </div>
                         <div class="title-container">
 
-                            <div class="persons"><a class="person" href="/about_br/nabiullinaes/"><img
-                                        src="https://optim.tildacdn.one/tild6463-3265-4636-b533-383765396565/-/cover/320x400/center/center/-/format/webp/photo.jpg" alt="Умурзаков Шавкат Буранович">
+                            <div class="persons"><a class="person" href="#!"><img
+                                        src="https://optim.tildacdn.one/tild6463-3265-4636-b533-383765396565/-/cover/320x400/center/center/-/format/webp/photo.jpg"
+                                        alt="Умурзаков Шавкат Буранович">
                                     <div class="person_name">Умурзаков Шавкат <br> Буранович
                                     </div>
-                                    <div class="person_position">Председатель наблюдательного совета<br>Хоким города Ташкента</div>
+                                    <div class="person_position">Председатель наблюдательного совета<br>Хоким города
+                                        Ташкента</div>
                                 </a>
                             </div>
 
                             <div class="persons">
-                                <span class="person" href="#"><img
-                                        src="https://optim.tildacdn.one/tild6533-3266-4265-b966-376336363163/-/cover/320x400/center/center/-/format/webp/__.jpg" alt="Скоробогатова Ольга">
+                                <a class="person" href="#!">
+                                    <img
+                                        src="https://optim.tildacdn.one/tild6533-3266-4265-b966-376336363163/-/cover/320x400/center/center/-/format/webp/__.jpg"
+                                        alt="Скоробогатова Ольга">
                                     <div class="person_name">Рахманов Шароф <br> Диерович
                                     </div>
                                     <div class="person_position">Член
                                         наблюдательного совета<br>Заместитель Хокима города Ташкента</div>
-                                </span>
+                                </a>
 
-                                <a class="person" href="/about_br/tulindv/">
-                                    <img
-                                        src="https://optim.tildacdn.one/tild3031-6336-4637-a639-346461626662/-/cover/320x400/center/center/-/format/webp/__.jpg" alt="Тулин Дмитрий">
+                                <a class="person" href="#!">
+                                    <img src="https://optim.tildacdn.one/tild3031-6336-4637-a639-346461626662/-/cover/320x400/center/center/-/format/webp/__.jpg"
+                                        alt="Тулин Дмитрий">
                                     <div class="person_name">Тогаев Наби <br> Исмоилович
                                     </div>
-                                    <div class="person_position">наблюдательного совета<br>Начальник отдела ООО «Узбекско-Оманская<br>Инвестиционная
+                                    <div class="person_position">наблюдательного совета<br>Начальник отдела ООО
+                                        «Узбекско-Оманская<br>Инвестиционная
                                         Компания»</div>
                                 </a>
 
 
-                                <a class="person" href="/about_br/chistyukhinvv/"><img
-                                        src="https://optim.tildacdn.one/tild6430-6239-4361-b062-313631343137/-/cover/320x400/center/center/-/format/webp/_-2.jpg" alt="Прияткин Алексей Николаевич">
+                                <a class="person" href="#!"><img
+                                        src="https://optim.tildacdn.one/tild6430-6239-4361-b062-313631343137/-/cover/320x400/center/center/-/format/webp/_-2.jpg"
+                                        alt="Прияткин Алексей Николаевич">
                                     <div class="person_name">Прияткин Алексей <br> Николаевич
                                     </div>
                                     <div class="person_position">Член
@@ -60,32 +66,6 @@
                         </div>
                     </div>
 
-
-                    <!--/index-->
-
-                    <div class="page-info">
-                        <!--noindex-->
-
-
-
-                        <div class="page-info_helpful" data-page-feedback=""
-                            data-feedback-url="/Content/PageInfo/RegisterFeedback">
-                            <!--noindex-->
-                            <div class="helpful" data-helpful="">
-                                Страница была полезной?
-                                <div class="helpful_btns">
-                                    <button class="helpful_btn _yes" data-helpful-btn="yes"><i
-                                            class="helpful_icon"></i><span>Да</span></button>
-                                    <button class="helpful_btn _no" data-helpful-btn="no"><i
-                                            class="helpful_icon"></i><span>Нет</span></button>
-                                </div>
-                            </div>
-                            <!--/noindex-->
-                        </div>
-
-                        <div class="page-info_last-update">Последнее обновление страницы: 05.12.2024</div>
-                        <!--/noindex-->
-                    </div>
 
                 </div>
             </div>
